35b09e6b780d9a1ecf5e99681262192ccd486c78
haeoro <73265375+haeoro@users.noreply.github.com>
2025-11-25 20:23:54 +1100

    Update main.cpp
    more notes.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 /*
-	A program that takes arguments (path),(privilege level) in order to run a program at the users desired level of privilege without having to 
-	have administrator privileges on the given account I.E, standard user. 
+	A program that takes arguments (path),(privilege level) in order to run a program at the users desired level of privilege without having to
+	have administrator privileges on the given account I.E, standard user.
 */
 
 #include <iostream>
 #include <windows.h>
 
-int main() 
+int main()
 {
-	SECURITY_DESCRIPTOR secDescriptor // we must use a function to manipulate, or set the data in this struct.
-	{
-	};
-	
+	SECURITY_DESCRIPTOR secDescriptor{};// we must use a function to manipulate, or set the data in this struct.
+
 	SECURITY_ATTRIBUTES secAttribs // this gets passed as a pointer to this struct as an argument to CreateProcessA() function.
 	{
 		sizeof(SECURITY_ATTRIBUTES),
@@ -20,20 +18,38 @@ int main()
 	};
 
 
+
 	STARTUPINFOA sInfo{ 0 }; // startup structure initialized to 0 to pass to CreateProcessA() (default)
 	PROCESS_INFORMATION pInfo{ 0 }; // proc info initialized to 0 to pass it to the CreateProcessA() structure (default)
-	
+
 
 	BOOL mainProc = CreateProcessA(
 		NULL,
 		(LPSTR)"C:\\Windows\\System32\\cmd.exe", // path to application to be run
 		&secAttribs, // pointer to SECURITY_ATTRIBUTES struct here (defines descriptor).
-		NULL, 
+		// we haven't done any customizations for anything else up to this point.
+		NULL,
 		FALSE,
 		CREATE_NEW_CONSOLE,
-		NULL, 
 		NULL,
-		&sInfo, 
+		NULL,
+		&sInfo,
 		&pInfo
 	);
 }
+
+
+/*
+		what i know.
+
+	when a user logs in, the os collects a set of data on the user that uniquely identifies the said user. 
+	It then stores the set in an access token. I think I should try my luck with trying to create a fake security 
+	descriptor. 
+
+		to-do
+
+	1. look into windows functions that define struct members for SECURITY_DESCRIPTOR instead of just initializing it with
+	default values.
+
+	2. reference that microsoft article that I emailed to myself.
+*/
